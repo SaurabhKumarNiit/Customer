c00c18a531f74661e49bee1c8c47de04058bda43
SAURABH KUMAR TIWARI <sky.royaltiwari@gmail.com>
2022-08-16 13:18:17 +0530

    Update_Attribute

--- src/data/Customer.java
+++ src/data/Customer.java
@@ -1,2 +1,9 @@
-package data;public class Customer {
+package data;
+
+public class Customer {
+    private String name;
+    private String address;
+    private String email;
+    private long mobile;
+    private boolean premiumMembership;
 }
